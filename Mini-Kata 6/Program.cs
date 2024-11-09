b67490e1919da81eac1884b59a1cdb9294c9a280
Caedamon <156080309+Caedamon@users.noreply.github.com>
2024-11-09 21:52:36 +0100

    Mini Kata 6 Start

---
+++ Mini-Kata 6/Program.cs
@@ -0,0 +1,5 @@
+﻿// Requirements
+//     Create an array of enemy names (e.g., "Goblin", "Orc", "Troll") and display each name using a loop.
+//     Create a list of player inventory items (e.g., "Sword", "Shield", "Potion") and display each item in the list using a loop.
+//     Add a new item to the inventory list (e.g., "Helmet") and display the updated list.
+
