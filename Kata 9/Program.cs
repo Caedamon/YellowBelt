3cae28a6f4ba7a99c755ec379e9cbe021ce9bec6
Caedamon <156080309+Caedamon@users.noreply.github.com>
2024-11-12 13:37:49 +0100

    K9

--- Kata 9/Program.cs
+++ Kata 9/Program.cs
@@ -15,28 +15,80 @@
 
 class Player
 {
-    
+    public string Name { get; set; }
+    public int Health { get; set; }
+    public int Level { get; set; }
+
+    public void Attack(Enemy enemy, int damage)
+    {
+        Console.WriteLine($"Filler");
+    }
 }
 
 class Enemy
 {
-    
+    public string Type { get; set; }
+    public int Health { get; set; }
+    public int Damage { get; set; }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        if (Health > 0)
+        {
+            Console.WriteLine($"filler");
+        }
+        else
+        {
+            Console.WriteLine($"Filler");
+        }
+    }
 }
 
 class NPC
 {
-    
+    public string Name { get; set; }
+    public string Dialogue { get; set; }
+
+    public void Speak()
+    {
+        Console.WriteLine($"Filler");
+    }
 }
 
 class Merchant
 {
-    
+    public string Name { get; set; }
+    public List<string> Inventory { get; set; }
+
+    public void Trade()
+    {
+        Console.WriteLine($"Filler");
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
+        Player player = new Player
+        {
+
+        };
         
+        Enemy enemy = new Enemy
+        {
+
+        };
+        
+        NPC npc = new NPC
+        {
+
+        };
+        
+        Merchant merchant = new Merchant
+        {
+
+        };
     }
 }
